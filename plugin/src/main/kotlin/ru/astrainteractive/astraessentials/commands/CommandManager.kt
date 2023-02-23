414187c2a6ad797d3ad9cb804d1ebe1407671b0a
makeevrserg <makeevrserg@gmail.com>
2023-02-23 13:25:52 +0300

    added command to handle itemFrames state

--- plugin/src/main/kotlin/ru/astrainteractive/astraessentials/commands/CommandManager.kt
+++ plugin/src/main/kotlin/ru/astrainteractive/astraessentials/commands/CommandManager.kt
@@ -1,11 +1,13 @@
 package ru.astrainteractive.astraessentials.commands
 
 import org.bukkit.Bukkit
+import org.bukkit.entity.ItemFrame
 import org.bukkit.entity.Player
 import ru.astrainteractive.astraessentials.AstraEssentials
 import ru.astrainteractive.astraessentials.events.sit.SitController
 import ru.astrainteractive.astraessentials.plugin.Permission
 import ru.astrainteractive.astralibs.commands.registerCommand
+import ru.astrainteractive.astralibs.commands.registerTabCompleter
 import ru.astrainteractive.astralibs.utils.HEX
 
 
@@ -27,6 +29,31 @@ class CommandManager {
             }.onSuccess {
                 val message = args.slice(1 until args.size).joinToString(" ")
                 it.value.sendMessage(message.HEX())
+            }
+        }
+
+        // atemframe isVisible isFixed radius
+        AstraEssentials.instance.registerTabCompleter("atemframe") {
+            when (this.args.size) {
+                1 -> listOf("isVisible")
+                2 -> listOf("isFixed")
+                3 -> listOf("radius")
+                else -> emptyList()
+
+            }
+
+        }
+        AstraEssentials.instance.registerCommand("atemframe") {
+            if (!Permission.AtemFrame.hasPermission(sender)) return@registerCommand
+            val player = sender as? Player ?: return@registerCommand
+            val isVisible = argument(0) { it == "true" }.successOrNull()?.value ?: true
+            val isFixed = argument(1) { it == "true" }.successOrNull()?.value ?: true
+            val radius = argument(2) { it?.toDoubleOrNull() }.successOrNull()?.value ?: 20.0
+            val itemFrames = player.location.getNearbyEntitiesByType(ItemFrame::class.java, radius)
+            player.sendMessage("Params: isVisible: $isVisible; isFixed: $isFixed; radius: $radius; changed ${itemFrames.size} frames")
+            itemFrames.forEach {
+                it.isFixed = isFixed
+                it.isVisible = isVisible
             }
         }
     }
--- plugin/src/main/kotlin/ru/astrainteractive/astraessentials/plugin/Permission.kt
+++ plugin/src/main/kotlin/ru/astrainteractive/astraessentials/plugin/Permission.kt
@@ -6,4 +6,6 @@ sealed class Permission(override val value: String) : IPermission {
     object Reload : Permission("astra_template.reload")
     object Damage : Permission("astra_template.damage")
     object TellChat : Permission("astra_template.tellchat")
+    object AtemFrame : Permission("astra_template.atemframe")
+
 }
