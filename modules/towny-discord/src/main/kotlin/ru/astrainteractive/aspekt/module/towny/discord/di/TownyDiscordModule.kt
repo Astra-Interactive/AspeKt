77a3a99efabcb58c6b0f18ad539448d2cb0b2b30
Roman Makeev <makeevrserg@gmail.com>
2024-01-20 18:14:18 +0300

    fix towny module

--- modules/towny-discord/src/main/kotlin/ru/astrainteractive/aspekt/module/towny/discord/di/TownyDiscordModule.kt
+++ modules/towny-discord/src/main/kotlin/ru/astrainteractive/aspekt/module/towny/discord/di/TownyDiscordModule.kt
@@ -1,5 +1,6 @@
 package ru.astrainteractive.aspekt.module.towny.discord.di
 
+import org.bukkit.Bukkit
 import ru.astrainteractive.aspekt.di.CoreModule
 import ru.astrainteractive.aspekt.job.ScheduledJob
 import ru.astrainteractive.aspekt.module.towny.discord.job.TownyDiscordRoleJob
@@ -9,22 +10,25 @@ interface TownyDiscordModule {
     val lifecycle: Lifecycle
 
     class Default(coreModule: CoreModule) : TownyDiscordModule {
-        private val job: ScheduledJob = TownyDiscordRoleJob(
-            scope = coreModule.scope.value,
-            dispatchers = coreModule.dispatchers.value,
-            configurationDependency = coreModule.pluginConfig
-        )
+        private val job: ScheduledJob? by lazy {
+            if (Bukkit.getPluginManager().getPlugin("Towny") == null) return@lazy null
+            TownyDiscordRoleJob(
+                scope = coreModule.scope.value,
+                dispatchers = coreModule.dispatchers.value,
+                configurationDependency = coreModule.pluginConfig
+            )
+        }
         override val lifecycle: Lifecycle by lazy {
             Lifecycle.Lambda(
                 onEnable = {
-                    job.onEnable()
+                    job?.onEnable()
                 },
                 onDisable = {
-                    job.onDisable()
+                    job?.onDisable()
                 },
                 onReload = {
-                    job.onDisable()
-                    job.onEnable()
+                    job?.onDisable()
+                    job?.onEnable()
                 }
 
             )
