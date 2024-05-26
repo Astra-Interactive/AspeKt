9a86070f9198e6a816c264f733237dd0901a7024
makeevrserg <makeevrserg@gmail.com>
2024-05-26 18:13:46 +0300

    fix swear regex

--- modules/antiswear/src/test/kotlin/ru/astrainteractive/aspekt/module/antiswear/util/SwearRuRegexTest.kt
+++ modules/antiswear/src/test/kotlin/ru/astrainteractive/aspekt/module/antiswear/util/SwearRuRegexTest.kt
@@ -1,19 +1,23 @@
 package ru.astrainteractive.aspekt.module.antiswear.util
 
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 import net.kyori.adventure.text.Component
 import org.junit.Test
 
 class SwearRuRegexTest {
     @Test
     fun GIVEN_swear_WHEN_regex_THEN_match() {
-        assert(SwearRuRegex.SWEAR_REGEX.matches("бля"))
-        assert(SwearRuRegex.SWEAR_REGEX.matches("еблан"))
+        assertTrue(SwearRuRegex.SWEAR_REGEX.matches("бля"))
+        assertTrue(SwearRuRegex.SWEAR_REGEX.matches("еблан"))
+        assertFalse(SwearRuRegex.SWEAR_REGEX.containsMatchIn("Привет как дела"))
+        assertTrue(SwearRuRegex.SWEAR_REGEX.containsMatchIn("Привет блять бля"))
     }
 
     @Test
     fun GIVEN_component_with_swears_WHEN_replace_by_regex_THEN_contains_replacement() {
         val component = Component.text("бля урод ебаный гандон")
         val replaced = component.replaceText(SwearRuRegex.REPLACEMENT_CONFIG)
-        assert(replaced.toString().contains(SwearRuRegex.REPLACEMENT_STRING))
+        assertTrue(replaced.toString().contains(SwearRuRegex.REPLACEMENT_STRING))
     }
 }
