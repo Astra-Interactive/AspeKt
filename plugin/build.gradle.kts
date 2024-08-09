be1b8a166e5a1aea17685c36d97e34e4a11ebfd4
makeevrserg <makeevrserg@gmail.com>
2024-08-09 19:01:45 +0300

    fix toml dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -21,9 +21,9 @@ dependencies {
     implementation(libs.minecraft.astralibs.core.bukkit)
     implementation(libs.minecraft.vaultapi)
     // Spigot
-    compileOnly("net.luckperms:api:5.4")
+    compileOnly(libs.minecraft.luckperms)
     compileOnly(libs.minecraft.discordsrv)
-    compileOnly("net.essentialsx:EssentialsX:2.20.1")
+    compileOnly(libs.minecraft.essentialsx)
     // Test
     testImplementation(libs.bundles.testing.kotlin)
     testImplementation(libs.tests.kotlin.test)
