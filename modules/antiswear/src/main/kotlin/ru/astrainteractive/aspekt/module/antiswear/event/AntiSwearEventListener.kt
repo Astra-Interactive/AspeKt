f3212c3c1e90c621c804558b355a2708bbaba47c
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2024-10-13 22:30:56 +0300

    fix swear filter (#93)

--- modules/antiswear/src/main/kotlin/ru/astrainteractive/aspekt/module/antiswear/event/AntiSwearEventListener.kt
+++ modules/antiswear/src/main/kotlin/ru/astrainteractive/aspekt/module/antiswear/event/AntiSwearEventListener.kt
@@ -10,14 +10,17 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.server.BroadcastMessageEvent
 import org.bukkit.plugin.Plugin
 import ru.astrainteractive.aspekt.module.antiswear.data.SwearRepository
+import ru.astrainteractive.aspekt.module.antiswear.util.SwearRuRegex
 import ru.astrainteractive.astralibs.event.EventListener
 
 internal class AntiSwearEventListener(
     private val swearRepository: SwearRepository,
     private val scope: CoroutineScope
 ) : EventListener {
+
     @EventHandler(priority = EventPriority.HIGHEST)
     fun onMessage(e: AsyncChatEvent) {
         val swearRenderer = SwearRenderer(
@@ -25,6 +28,11 @@ internal class AntiSwearEventListener(
             swearRepository = swearRepository
         )
         e.renderer(swearRenderer)
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    fun onBroadcastMessage(e: BroadcastMessageEvent) {
+        e.message(e.message().replaceText(SwearRuRegex.REPLACEMENT_CONFIG))
     }
 
     @EventHandler
