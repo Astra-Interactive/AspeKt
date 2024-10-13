14f590a74874a5e43c1908f885642ce9168d7324
Roman Makeev <makeevrserg@gmail.com>
2024-10-13 22:27:45 +0300

    fix swear filter

--- modules/antiswear/src/main/kotlin/ru/astrainteractive/aspekt/module/antiswear/event/SwearRenderer.kt
+++ modules/antiswear/src/main/kotlin/ru/astrainteractive/aspekt/module/antiswear/event/SwearRenderer.kt
@@ -3,14 +3,17 @@ package ru.astrainteractive.aspekt.module.antiswear.event
 import io.papermc.paper.chat.ChatRenderer
 import net.kyori.adventure.audience.Audience
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.bukkit.entity.Player
 import ru.astrainteractive.aspekt.module.antiswear.data.SwearRepository
 import ru.astrainteractive.aspekt.module.antiswear.util.SwearRuRegex
+import ru.astrainteractive.astralibs.logging.JUtiltLogger
+import ru.astrainteractive.astralibs.logging.Logger
 
 internal class SwearRenderer(
     private val renderer: ChatRenderer,
     private val swearRepository: SwearRepository
-) : ChatRenderer {
+) : ChatRenderer, Logger by JUtiltLogger("AspeKt-SwearRenderer") {
     override fun render(
         source: Player,
         sourceDisplayName: Component,
@@ -20,6 +23,13 @@ internal class SwearRenderer(
         val originalMessage = renderer.render(source, sourceDisplayName, message, viewer)
         val player = viewer as? Player ?: return originalMessage
         if (!swearRepository.isSwearFilterEnabled(player)) return originalMessage
-        return originalMessage.replaceText(SwearRuRegex.REPLACEMENT_CONFIG)
+        val newText = originalMessage.replaceText(SwearRuRegex.REPLACEMENT_CONFIG)
+
+        info {
+            "#render swear for player is disabled. rendering: ${
+                PlainTextComponentSerializer.plainText().serialize(originalMessage)
+            } -> ${PlainTextComponentSerializer.plainText().serialize(newText)}."
+        }
+        return newText
     }
 }
--- modules/antiswear/src/test/kotlin/ru/astrainteractive/aspekt/module/antiswear/util/SwearRuRegexTest.kt
+++ modules/antiswear/src/test/kotlin/ru/astrainteractive/aspekt/module/antiswear/util/SwearRuRegexTest.kt
@@ -1,14 +1,20 @@
 package ru.astrainteractive.aspekt.module.antiswear.util
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class SwearRuRegexTest {
+    private fun Component.toPlain(): String {
+        return PlainTextComponentSerializer.plainText().serialize(this)
+    }
+
     @Test
     fun GIVEN_swear_WHEN_regex_THEN_match() {
-        assert(SwearRuRegex.SWEAR_REGEX.matches("бля"))
-        assert(SwearRuRegex.SWEAR_REGEX.matches("еблан"))
-        assert(!SwearRuRegex.SWEAR_REGEX.matches("Привет друг"))
+        assert(SwearRuRegex.SWEAR_REGEX.toRegex().matches("бля"))
+        assert(SwearRuRegex.SWEAR_REGEX.toRegex().matches("еблан"))
+        assert(!SwearRuRegex.SWEAR_REGEX.toRegex().matches("Привет друг"))
     }
 
     @Test
@@ -16,5 +22,15 @@ class SwearRuRegexTest {
         val component = Component.text("бля урод ебаный гандон")
         val replaced = component.replaceText(SwearRuRegex.REPLACEMENT_CONFIG)
         assert(replaced.toString().contains(SwearRuRegex.REPLACEMENT_STRING))
+    }
+
+    @Test
+    @Suppress("MaxLineLength", "MaximumLineLength")
+    fun GIVEN_component_with_swears_caps_WHEN_replace_by_regex_THEN_contains_replacement() {
+        val component = Component.text("бля БЛЯ ХУЙ хуй")
+        val replaced = component.replaceText(SwearRuRegex.REPLACEMENT_CONFIG)
+        assert(replaced.toString().contains(SwearRuRegex.REPLACEMENT_STRING))
+        val expect = "${SwearRuRegex.REPLACEMENT_STRING} ${SwearRuRegex.REPLACEMENT_STRING} ${SwearRuRegex.REPLACEMENT_STRING} ${SwearRuRegex.REPLACEMENT_STRING}"
+        assertEquals(expect, replaced.toPlain())
     }
 }
