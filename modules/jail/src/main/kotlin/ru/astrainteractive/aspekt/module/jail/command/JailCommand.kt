594dba2668139ecd580f82278ab0b2973d1f3bba
Roman Makeev <makeevrserg@gmail.com>
2025-03-25 14:01:34 +0300

    refactor commmand

--- modules/jail/src/main/kotlin/ru/astrainteractive/aspekt/module/jail/command/JailCommand.kt
+++ modules/jail/src/main/kotlin/ru/astrainteractive/aspekt/module/jail/command/JailCommand.kt
@@ -17,6 +17,8 @@ import ru.astrainteractive.astralibs.command.api.argumenttype.EnumArgument
 import ru.astrainteractive.astralibs.command.api.argumenttype.EnumArgumentType
 import ru.astrainteractive.astralibs.command.api.argumenttype.OfflinePlayerArgument
 import ru.astrainteractive.astralibs.command.api.argumenttype.StringArgumentType
+import ru.astrainteractive.astralibs.command.api.context.BukkitCommandContext
+import ru.astrainteractive.astralibs.command.api.context.BukkitCommandContextExt
 import ru.astrainteractive.astralibs.command.api.context.BukkitCommandContextExt.requireArgument
 import ru.astrainteractive.astralibs.command.api.context.BukkitCommandContextExt.requirePermission
 import ru.astrainteractive.astralibs.command.api.exception.StringDescException
@@ -46,6 +48,124 @@ internal enum class JailArg(override val value: String) : EnumArgument {
     FREE("free")
 }
 
+private fun JailCommandManager.listJails(ctx: BukkitCommandContext) {
+    ctx.requirePermission(PluginPermission.Jail.JailList)
+    val translation = translationKrate.cachedValue
+    scope.launch {
+        with(kyoriKrate.cachedValue) {
+            val jails = jailApi.getJails().getOrNull().orEmpty()
+            val jailsString = jails.joinToString()
+            ctx.sender.sendMessage(translation.jails.jailsList(jailsString).component)
+        }
+    }
+}
+
+private fun JailCommandManager.createJail(ctx: BukkitCommandContext) {
+    ctx.requirePermission(PluginPermission.Jail.JailCreate)
+    val player = ctx.sender as? Player
+    player ?: throw StringDescException(StringDesc.Plain("Executor should be player"))
+    val jail = Jail(
+        name = ctx.requireArgument(1, StringArgumentType),
+        location = JailLocation(
+            world = player.location.world.name,
+            x = player.location.x,
+            y = player.location.y,
+            z = player.location.z
+        )
+    )
+    val translation = translationKrate.cachedValue
+    scope.launch {
+        with(kyoriKrate.cachedValue) {
+            jailApi.addJail(jail)
+                .onFailure {
+                    error(it) { "#JailArg.CREATE" }
+                    ctx.sender.sendMessage(translation.jails.jailCreatedFail.component)
+                }
+                .onSuccess {
+                    scope.launch {
+                        ctx.sender.sendMessage(translation.jails.jailCreatedSuccess(jail.name).component)
+                    }
+                }
+        }
+    }
+}
+
+private fun JailCommandManager.deleteJail(ctx: BukkitCommandContext) {
+    ctx.requirePermission(PluginPermission.Jail.JailDelete)
+    val translation = translationKrate.cachedValue
+    scope.launch {
+        val jailName = ctx.requireArgument(1, StringArgumentType)
+        with(kyoriKrate.cachedValue) {
+            jailApi.deleteJail(jailName)
+                .onFailure {
+                    error(it) { "#JailArg.CREATE" }
+                    ctx.sender.sendMessage(translation.jails.jailDeleteFail.component)
+                }
+                .onSuccess {
+                    scope.launch {
+                        ctx.sender.sendMessage(translation.jails.jailDeleteSuccess(jailName).component)
+                    }
+                }
+        }
+    }
+}
+
+private fun JailCommandManager.inmateIntoJail(ctx: BukkitCommandContext) {
+    ctx.requirePermission(PluginPermission.Jail.JailInmate)
+    val translation = translationKrate.cachedValue
+    scope.launch {
+        val jailOfflinePlayer = ctx.requireArgument(1, OfflinePlayerArgument)
+        val jailName = ctx.requireArgument(2, StringArgumentType)
+        val jailDuration = ctx.requireArgument(3, DurationArgumentType)
+        with(kyoriKrate.cachedValue) {
+            val inmate = JailInmate(
+                uuid = jailOfflinePlayer.uniqueId.toString(),
+                jailName = jailName,
+                start = Instant.now(),
+                duration = jailDuration
+            )
+            jailApi.addInmate(inmate)
+                .onFailure {
+                    error(it) { "#JailArg.CREATE" }
+                    ctx.sender.sendMessage(translation.jails.inmateAddFail.component)
+                }
+                .onSuccess {
+                    scope.launch {
+                        ctx.sender.sendMessage(
+                            translation.jails.inmateAddSuccess(
+                                name = jailOfflinePlayer.name.orEmpty(),
+                                jail = jailName
+                            ).component
+                        )
+                    }
+                }
+        }
+    }
+}
+
+private fun JailCommandManager.freeFromJail(ctx: BukkitCommandContext) {
+    ctx.requirePermission(PluginPermission.Jail.JailFree)
+    val translation = translationKrate.cachedValue
+    scope.launch {
+        val jailOfflinePlayer = ctx.requireArgument(1, OfflinePlayerArgument)
+        with(kyoriKrate.cachedValue) {
+            jailApi.free(jailOfflinePlayer.uniqueId.toString())
+                .onFailure {
+                    error(it) { "#JailArg.CREATE" }
+                    ctx.sender.sendMessage(translation.jails.inmateFreeFail.component)
+                }
+                .onSuccess {
+                    scope.launch {
+                        ctx.sender.sendMessage(
+                            translation.jails.inmateFreeSuccess(
+                                name = jailOfflinePlayer.name.orEmpty(),
+                            ).component
+                        )
+                    }
+                }
+        }
+    }
+}
 
 /**
  * /jail list
@@ -56,128 +176,20 @@ internal enum class JailArg(override val value: String) : EnumArgument {
 internal fun JailCommandManager.jail() = plugin.setCommandExecutor(
     alias = "jail",
     commandExecutor = commandExecutor@{ ctx ->
-
         val jailArg = ctx.requireArgument(0, EnumArgumentType(JailArg.entries))
         when (jailArg) {
-            JailArg.LIST -> {
-                ctx.requirePermission(PluginPermission.Jail.JailList)
-                val translation = translationKrate.cachedValue
-                scope.launch {
-                    with(kyoriKrate.cachedValue) {
-                        val jails = jailApi.getJails().getOrNull().orEmpty()
-                        val jailsString = jails.joinToString()
-                        ctx.sender.sendMessage(translation.jails.jailsList(jailsString).component)
-                    }
-                }
-            }
+            JailArg.LIST -> listJails(ctx)
 
-            JailArg.CREATE -> {
-                ctx.requirePermission(PluginPermission.Jail.JailCreate)
-                val player = ctx.sender as? Player
-                player ?: throw StringDescException(StringDesc.Plain("Executor should be player"))
-                val jail = Jail(
-                    name = ctx.requireArgument(1, StringArgumentType),
-                    location = JailLocation(
-                        world = player.location.world.name,
-                        x = player.location.x,
-                        y = player.location.y,
-                        z = player.location.z
-                    )
-                )
-                val translation = translationKrate.cachedValue
-                scope.launch {
-                    with(kyoriKrate.cachedValue) {
-                        jailApi.addJail(jail)
-                            .onFailure {
-                                error(it) { "#JailArg.CREATE" }
-                                ctx.sender.sendMessage(translation.jails.jailCreatedFail.component)
-                            }
-                            .onSuccess {
-                                scope.launch {
-                                    ctx.sender.sendMessage(translation.jails.jailCreatedSuccess(jail.name).component)
-                                }
-                            }
-                    }
-                }
-            }
+            JailArg.CREATE -> createJail(ctx)
 
-            JailArg.DELETE -> {
-                ctx.requirePermission(PluginPermission.Jail.JailDelete)
-                val translation = translationKrate.cachedValue
-                scope.launch {
-                    val jailName = ctx.requireArgument(1, StringArgumentType)
-                    with(kyoriKrate.cachedValue) {
-                        jailApi.deleteJail(jailName)
-                            .onFailure {
-                                error(it) { "#JailArg.CREATE" }
-                                ctx.sender.sendMessage(translation.jails.jailDeleteFail.component)
-                            }
-                            .onSuccess {
-                                scope.launch {
-                                    ctx.sender.sendMessage(translation.jails.jailDeleteSuccess(jailName).component)
-                                }
-                            }
-                    }
-                }
-            }
+            JailArg.DELETE -> deleteJail(ctx)
 
-            JailArg.INMATE -> {
-                ctx.requirePermission(PluginPermission.Jail.JailInmate)
-                val translation = translationKrate.cachedValue
-                scope.launch {
-                    val jailOfflinePlayer = ctx.requireArgument(1, OfflinePlayerArgument)
-                    val jailName = ctx.requireArgument(2, StringArgumentType)
-                    val jailDuration = ctx.requireArgument(3, DurationArgumentType)
-                    with(kyoriKrate.cachedValue) {
-                        val inmate = JailInmate(
-                            uuid = jailOfflinePlayer.uniqueId.toString(),
-                            jailName = jailName,
-                            start = Instant.now(),
-                            duration = jailDuration
-                        )
-                        jailApi.addInmate(inmate)
-                            .onFailure {
-                                error(it) { "#JailArg.CREATE" }
-                                ctx.sender.sendMessage(translation.jails.inmateAddFail.component)
-                            }
-                            .onSuccess {
-                                scope.launch {
-                                    ctx.sender.sendMessage(
-                                        translation.jails.inmateAddSuccess(
-                                            name = jailOfflinePlayer.name.orEmpty(),
-                                            jail = jailName
-                                        ).component
-                                    )
-                                }
-                            }
-                    }
-                }
-            }
+            JailArg.INMATE -> inmateIntoJail(ctx)
 
-            JailArg.FREE -> {
-                ctx.requirePermission(PluginPermission.Jail.JailFree)
-                val translation = translationKrate.cachedValue
-                scope.launch {
-                    val jailOfflinePlayer = ctx.requireArgument(1, OfflinePlayerArgument)
-                    with(kyoriKrate.cachedValue) {
-                        jailApi.free(jailOfflinePlayer.uniqueId.toString())
-                            .onFailure {
-                                error(it) { "#JailArg.CREATE" }
-                                ctx.sender.sendMessage(translation.jails.inmateFreeFail.component)
-                            }
-                            .onSuccess {
-                                scope.launch {
-                                    ctx.sender.sendMessage(
-                                        translation.jails.inmateFreeSuccess(
-                                            name = jailOfflinePlayer.name.orEmpty(),
-                                        ).component
-                                    )
-                                }
-                            }
-                    }
-                }
-            }
+            JailArg.FREE -> freeFromJail(ctx)
         }
     },
-    errorHandler = { ctx, throwable -> }
+    errorHandler = { ctx, throwable ->
+        error(throwable) { "could not execute jail command" }
+    }
 )
