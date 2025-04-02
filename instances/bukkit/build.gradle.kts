d0c636a5530a7bb6b6c1c602d2d7e06bef9b36c6
Roman Makeev <makeevrserg@gmail.com>
2025-04-02 12:21:55 +0300

    fix moneydrop

--- instances/bukkit/build.gradle.kts
+++ instances/bukkit/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation(projects.modules.entities)
     implementation(projects.modules.jail)
 }
-val destination = File("/home/makeevrserg/Desktop/git/conf/conf.smp/smp/plugins/")
+val destination = File("/home/makeevrserg/Desktop/git/conf.smp/smp/plugins/")
     .takeIf(File::exists)
     ?: File(rootDir, "jars")
 
